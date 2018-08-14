d1e87102e43d892a98f04c1d4e984caf712829cc
Kevin Gawo <kevgawo@gmil.com>
2018-08-14 23:08:37 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var port = process.env.port || 5000 ;
+
+
+app.use('/', express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html', {root: path.join(__dirname, 'public')})
+});
+
+
+
+app.listen(port)  
